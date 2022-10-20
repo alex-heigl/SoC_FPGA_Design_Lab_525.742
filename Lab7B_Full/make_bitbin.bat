bd97912426d4124e1404f38d2d0ca1ad4f61ecfb
Alexander Heigl <aheigl2@jh.edu>
2022-10-19 19:23:02 -0500

    Syncing to laptop

--- Lab7B_Full/make_bitbin.bat
+++ Lab7B_Full/make_bitbin.bat
@@ -1,5 +1,4 @@
 : this file assumes you have a bitfile in the impl_1 directory, and turns it into a bit.bin file
 : for fpgautil
-call c:\Xilinx\Vivado\2022.1\settings64.bat
+call c:\Xilinx\Vivado\2022.156\settings64.bat
 call bootgen -image alex.bif -arch zynq -process_bitstream bin -w on
-
